82b3aeea9dabc7140e7aba6bf71f1cace8b9eb49
adamhamdi85 <adamhamdi85@gmail.com>
2022-10-10 15:49:00 +0100

    README.md added with Python variables lesson

---
+++ README.md
@@ -0,0 +1,9 @@
+# Python Intro
+## Why Python
+### Python use cases
+#### Python set up with Pycharm
+##### Python Variables
+
+-Env Testing print("hello world")
+
+```python
---
+++ env_testing.py
@@ -0,0 +1,31 @@
+
+# first_name = "Adam"
+# last_name = "Hamdi"
+# salary = 50
+# travel= 3.5
+#
+# print(first_name)
+# print(last_name)
+# print(salary)
+# print(travel)
+#
+# print(type(last_name))
+#
+# print("good morning, please enter your name ")
+#
+#
+# print(name)
+# print("hello dear ")
+
+
+
+
+
+print("what is your first name? ")
+first_name = input()
+print(" what is your last name? ")
+last_name = input()
+print(" what is your DOB? ")
+DOB = input()
+print(" are you a UK resident? ")
+UK_resident = input()

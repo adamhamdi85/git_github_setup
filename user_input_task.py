8647a348d6b037d3c333bcc0db13811534f3e2e2
adamhamdi85 <adamhamdi85@gmail.com>
2022-10-11 14:31:49 +0100

    user task

---
+++ user_input_task.py
@@ -0,0 +1,15 @@
+print("Good Morning, Please enter your First Name")
+first_name = input()
+#
+print ("Enter Last Name")
+last_name = input()
+#
+print("Enter DOB")
+DOB = input()
+
+print("Are you a Uk resident? Answer Yes or No")
+
+uk_resident = input()
+
+
+print(f'Hello   {first_name}  {last_name} {DOB} {uk_resident}')
